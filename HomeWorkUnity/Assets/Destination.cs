dbacad576aeee05d51904c375808c5974a7523a8
happy30 <jordivdh@hotmail.com>
2016-10-03 13:35:56 +0200

    no message

---
+++ HomeWorkUnity/Assets/Destination.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destination : MonoBehaviour {
+
+    NavMeshAgent nav;
+    public Transform dest;
+
+	// Use this for initialization
+	void Start ()
+    {
+        nav = GetComponent<NavMeshAgent>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        nav.SetDestination(dest.position);
+	}
+}
